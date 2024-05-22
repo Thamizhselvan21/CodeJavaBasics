45870adbb7a5153b35af8dea7605ca7bdf3ab476
TNS INDIA <TNS INDIA@Thamizh-TNS>
2024-05-22 20:35:46 +0530

    i added one print line

--- CoreJavaBasics/src/Basics/Addition.java
+++ CoreJavaBasics/src/Basics/Addition.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Addition {
 
 	public static void main(String[] args) {
+		System.out.println("Finland");
 		int a,b,c;
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the values:");
