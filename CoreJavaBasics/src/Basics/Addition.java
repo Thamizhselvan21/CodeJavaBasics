9e97a099872673ea4ce924857c869dd01c1a0cb9
TNS INDIA <TNS INDIA@Thamizh-TNS>
2024-05-22 20:25:16 +0530

    This project is reference for core java basics

---
+++ CoreJavaBasics/src/Basics/Addition.java
@@ -0,0 +1,17 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the values:");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Addition of a="+a+" and b="+b+" is:"+c);
+	}
+
+}
